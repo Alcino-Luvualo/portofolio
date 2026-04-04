a109a9651d13b2736fa2a5106fd363f1461b9c01
alcino-luvualo <alcinoluvualo@gmail.com>
2026-04-04 06:27:34 +0100

    feat(icons): add tech icon set

---
+++ src/components/TechIcons.tsx
@@ -0,0 +1,155 @@
+export const TechIcons = {
+  React: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32" fill="none">
+      <circle cx="16" cy="16" r="2.5" fill="#61DAFB"/>
+      <path d="M16 8.5c4.5 0 8.5 1 8.5 2s-4 2-8.5 2-8.5-1-8.5-2 4-2 8.5-2z" stroke="#61DAFB" strokeWidth="1.5" fill="none"/>
+      <path d="M11 11c2.5-4 5.5-6.5 7-6s2 4 0 9-5.5 6.5-7 6-2-4 0-9z" stroke="#61DAFB" strokeWidth="1.5" fill="none"/>
+      <path d="M11 21c-2.5-4-2-8.5-.5-9s4.5 2 7 6 2 8.5.5 9-4.5-2-7-6z" stroke="#61DAFB" strokeWidth="1.5" fill="none"/>
+    </svg>
+  ),
+  Angular: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32" fill="none">
+      <path d="M16 2L3 7l2 17 11 6 11-6 2-17L16 2z" fill="#DD0031"/>
+      <path d="M16 2v28l11-6 2-17L16 2z" fill="#C3002F"/>
+      <path d="M16 6l-7 16h3l1.5-4h5l1.5 4h3L16 6zm0 5l2 5h-4l2-5z" fill="#fff"/>
+    </svg>
+  ),
+  TypeScript: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <rect width="32" height="32" rx="4" fill="#3178C6"/>
+      <path d="M18 20v6h2v-6h-2zm-4-8h8v2h-3v12h-2V14h-3v-2z" fill="#fff"/>
+    </svg>
+  ),
+  JavaScript: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <rect width="32" height="32" rx="4" fill="#F7DF1E"/>
+      <path d="M18 20c0 1.5.5 2 1.5 2s1.5-.5 1.5-2v-8h2v8c0 2.5-1.5 4-3.5 4s-3.5-1.5-3.5-4h2zm-6 2c1 0 1.5-.5 1.5-1.5 0-1-.5-1.5-1.5-1.5h-1v-2h1c.8 0 1.2-.4 1.2-1.2s-.4-1.3-1.2-1.3c-.8 0-1.3.5-1.3 1.3H9c0-1.8 1.2-3 3-3s3 1.2 3 3c0 1-.5 1.8-1.2 2.2.7.4 1.2 1.2 1.2 2.3 0 1.8-1.2 3-3 3s-3-1.2-3-3h2c0 .5.5 1 1 1z" fill="#000"/>
+    </svg>
+  ),
+  Tailwind: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M9 13.7q1.4-5.6 7-5.6c5.6 0 6.3 4.2 9.1 4.9q2.8.7 4.9-2.1-1.4 5.6-7 5.6c-5.6 0-6.3-4.2-9.1-4.9Q11.1 10.9 9 13.7zM2 22.1q1.4-5.6 7-5.6c5.6 0 6.3 4.2 9.1 4.9q2.8.7 4.9-2.1-1.4 5.6-7 5.6c-5.6 0-6.3-4.2-9.1-4.9Q4.1 19.3 2 22.1z" fill="#06B6D4"/>
+    </svg>
+  ),
+  HTML5: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M6 28L4 3h24l-2 25-10 3z" fill="#E44D26"/>
+      <path d="M26 5H16v25l8-2.5z" fill="#F16529"/>
+      <path d="M9.5 17.5L9 14.5h14l-.5 3H13l.5 3.5h9L22 24l-6 2-6-2-.5-4h3l.5 2 3 1 3-1 .5-3h-10z" fill="#fff"/>
+    </svg>
+  ),
+  CSS3: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M6 28L4 3h24l-2 25-10 3z" fill="#1572B6"/>
+      <path d="M26 5H16v25l8-2.5z" fill="#33A9DC"/>
+      <path d="M9.5 17.5L9 14.5h14l-.5 3H13l.5 3.5h9L22 24l-6 2-6-2-.5-4h3l.5 2 3 1 3-1 .5-3h-10z" fill="#fff"/>
+    </svg>
+  ),
+  NodeJS: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 2L3 9v14l13 7 13-7V9z" fill="#68A063"/>
+      <path d="M16 2v28l13-7V9z" fill="#5A8F51"/>
+    </svg>
+  ),
+  AdonisJS: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 2l-4 8 4 8 4-8z" fill="#5A45FF"/>
+      <path d="M8 18l4-8-4 18h16l-4-18-4 8z" fill="#5A45FF" opacity="0.7"/>
+    </svg>
+  ),
+  Python: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 2c-2.5 0-4 1.5-4 4v2h4v.5H8c-2 0-4 1.5-4 4v6c0 2 1.5 4 4 4h2v-3c0-2 1.5-4 4-4h8c2 0 3.5-1.5 3.5-3.5V6c0-2.5-1.5-4-4-4h-5.5zm-1 2.5c.8 0 1.5.7 1.5 1.5s-.7 1.5-1.5 1.5-1.5-.7-1.5-1.5.7-1.5 1.5-1.5z" fill="#3776AB"/>
+      <path d="M16 30c2.5 0 4-1.5 4-4v-2h-4v-.5h8c2 0 4-1.5 4-4v-6c0-2-1.5-4-4-4h-2v3c0 2-1.5 4-4 4H10c-2 0-3.5 1.5-3.5 3.5V26c0 2.5 1.5 4 4 4h5.5zm1-2.5c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5 1.5.7 1.5 1.5-.7 1.5-1.5 1.5z" fill="#FFD43B"/>
+    </svg>
+  ),
+  Flask: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M13 4h6v8l6 12c1 2-1 4-3 4H10c-2 0-4-2-3-4l6-12V4z" fill="#000" stroke="#fff" strokeWidth="1"/>
+    </svg>
+  ),
+  MySQL: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M8 20c0 2 1 4 3 4h10c2 0 3-2 3-4V12c0-2-1-4-3-4H11c-2 0-3 2-3 4v8z" fill="#00758F"/>
+      <path d="M24 12v8c0 2-1 4-3 4h2c2 0 3-2 3-4v-8c0-2-1-4-3-4h-2c2 0 3 2 3 4z" fill="#00546B"/>
+      <circle cx="12" cy="14" r="1.5" fill="#fff"/>
+      <circle cx="20" cy="14" r="1.5" fill="#fff"/>
+    </svg>
+  ),
+  PostgreSQL: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 4c-4 0-7 2-7 6v8c0 4 3 6 7 6s7-2 7-6v-8c0-4-3-6-7-6z" fill="#336791"/>
+      <ellipse cx="16" cy="10" rx="7" ry="3" fill="#fff" opacity="0.3"/>
+      <path d="M12 24c0 2 1.5 4 4 4s4-2 4-4" stroke="#336791" strokeWidth="2" fill="none"/>
+    </svg>
+  ),
+  Git: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M30.5 14.5l-13-13c-.6-.6-1.5-.6-2.1 0l-13 13c-.6.6-.6 1.5 0 2.1l13 13c.6.6 1.5.6 2.1 0l13-13c.6-.6.6-1.5 0-2.1z" fill="#F05032"/>
+      <path d="M12 11l4 4-4 4v-8zm8 0v8l-4-4 4-4z" fill="#fff"/>
+    </svg>
+  ),
+  GitHub: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path fillRule="evenodd" clipRule="evenodd" d="M16 2C8.3 2 2 8.3 2 16c0 6.2 4 11.4 9.6 13.3.7.1 1-.3 1-.7v-2.6c-3.9.8-4.7-1.9-4.7-1.9-.6-1.6-1.5-2-1.5-2-1.2-.8.1-.8.1-.8 1.4.1 2.1 1.4 2.1 1.4 1.2 2.1 3.2 1.5 4 1.1.1-.9.5-1.5.9-1.8-3-.3-6.1-1.5-6.1-6.7 0-1.5.5-2.7 1.4-3.7-.1-.3-.6-1.6.1-3.3 0 0 1.1-.4 3.7 1.4 1.1-.3 2.2-.4 3.3-.4s2.2.1 3.3.4c2.6-1.8 3.7-1.4 3.7-1.4.7 1.7.2 3 .1 3.3.9 1 1.4 2.2 1.4 3.7 0 5.2-3.2 6.4-6.2 6.7.5.4.9 1.2.9 2.5v3.7c0 .4.3.8 1 .7C26 27.4 30 22.2 30 16c0-7.7-6.3-14-14-14z" fill="#fff"/>
+    </svg>
+  ),
+  Docker: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 12h3v3h-3zm-4 0h3v3h-3zm-4 0h3v3H8zm8-4h3v3h-3zm-4 0h3v3h-3z" fill="#2496ED"/>
+      <path d="M28 15c-1-1-3-1-4 0-1-3-3-5-6-5h-1v-1c0-1-1-2-2-2s-2 1-2 2v1H4c-1 0-2 1-2 2v1c0 5 4 9 9 9h6c5 0 9-4 9-9 1 0 2-1 2-2v-1c0-1 0-1 0 0z" fill="#2496ED"/>
+    </svg>
+  ),
+  AWS: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M6 18l10 6 10-6-10-6z" fill="#FF9900"/>
+      <path d="M6 14l10-6 10 6-10 6z" fill="#FFCC00" opacity="0.8"/>
+      <path d="M6 22l10 6V24l-10-6z" fill="#FF9900" opacity="0.6"/>
+      <path d="M26 22l-10 6V24l10-6z" fill="#FF9900" opacity="0.6"/>
+    </svg>
+  ),
+  Jest: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M22 4h-4l-2 8-2-8H10l4 16c0 2 1.5 4 4 4s4-2 4-4l4-16z" fill="#C21325"/>
+      <circle cx="16" cy="24" r="2" fill="#fff"/>
+    </svg>
+  ),
+  MongoDB: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 4c-.5 0-.9.2-1.2.5L6 14.3c-.3.3-.5.7-.5 1.2v9c0 .9.7 1.5 1.5 1.5h18c.8 0 1.5-.6 1.5-1.5v-9c0-.5-.2-.9-.5-1.2L17.2 4.5c-.3-.3-.7-.5-1.2-.5z" fill="#47A248"/>
+      <path d="M16 4v24" stroke="#fff" strokeWidth="1.5"/>
+    </svg>
+  ),
+  Firebase: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M6 26l4-24 6 11 4-7 6 20z" fill="#FFA611"/>
+      <path d="M6 26l10-6 10 6-10 4z" fill="#F57C00"/>
+      <path d="M16 20L10 8l6 12z" fill="#FFCA28"/>
+    </svg>
+  ),
+  Vitest: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 4l-8 14h16z" fill="#729B1B"/>
+      <path d="M16 4l8 14H8z" fill="#FCC72B" opacity="0.8"/>
+      <circle cx="16" cy="22" r="4" fill="#729B1B"/>
+    </svg>
+  ),
+  Vite: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M28 4L16 28 4 4l12 8z" fill="url(#vite-gradient)"/>
+      <defs>
+        <linearGradient id="vite-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" stopColor="#41D1FF"/>
+          <stop offset="100%" stopColor="#BD34FE"/>
+        </linearGradient>
+      </defs>
+    </svg>
+  ),
+  Webpack: () => (
+    <svg className="w-5 h-5" viewBox="0 0 32 32">
+      <path d="M16 4L4 10v12l12 6 12-6V10z" fill="#8DD6F9"/>
+      <path d="M16 4v24l12-6V10z" fill="#1C78C0"/>
+      <path d="M16 16l-8-4v8l8 4z" fill="#fff" opacity="0.3"/>
+    </svg>
+  ),
+};
