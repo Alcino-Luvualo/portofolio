bea8e5e0ae5f949c528cd3bbe9e540b770e448b4
alcino-luvualo <alcinoluvualo@gmail.com>
2026-04-06 14:08:57 +0100

    update project list

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,5 @@
 import Typewriter from "typewriter-effect";
 import { useLanguage } from "@/contexts/LanguageContext";
-import MagneticLink from "@/components/motion/MagneticLink";
 import CountUpStat from "@/components/motion/CountUpStat";
 
 export default function Hero() {
